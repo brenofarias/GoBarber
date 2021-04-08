26f7972677df804a6754b6191f826a52c21c5df5
Breno Farias <brenofarias.sousa@gmail.com>
2021-04-08 17:13:28 -0300

    Initial commit

---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('🐱‍🏍 Back-end started in 3333 port!');
+});
