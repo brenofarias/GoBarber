0b0ed3282635e3d46ced460d06f91c92e042a546
Breno Farias <brenofarias.sousa@gmail.com>
2021-04-10 13:22:10 -0300

    Model de agendamento

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,14 +1,10 @@
 import { Router } from 'express';
-import { v4 as uuid } from 'uuid';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
 
-const appointmentsRouter = Router();
+// Models
+import Appointment from '../models/Appointment';
 
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
+const appointmentsRouter = Router();
 
 const appointments: Appointment[] = [];
 
@@ -25,11 +21,7 @@ appointmentsRouter.post('/', (req, res) => {
     return res.status(400).json({ message: 'This appointment is alredy booked' });
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate
-  }
+  const appointment = new Appointment(provider, parsedDate);
 
   appointments.push(appointment);
 
