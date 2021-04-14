844641ebf983af4e0ca0953a43e237b007482435
Breno Farias <brenofarias.sousa@gmail.com>
2021-04-13 22:25:53 -0300

    Relacionamento nos models

--- backend/src/database/migrations/1618360300448-CreateUsers.ts
+++ backend/src/database/migrations/1618360300448-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1618360300448 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
