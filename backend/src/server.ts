9ca5abd8dbbef010f25fbe27d9157151c18ca0d8
Breno Farias <brenofarias.sousa@gmail.com>
2021-04-15 18:09:34 -0300

    Lidando com erros

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,33 @@
 import 'reflect-metadata';
-import express from 'express';
+
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 
 import uploadConfig from './config/upload';
 import routes from './routes';
+import AppError from './errors/AppError';
+
 import './database';
 
 const app = express();
 
 app.use(express.json());
-
 app.use('/files', express.static(uploadConfig.directory));
-
 app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res
+      .status(err.statusCode)
+      .json({ status: 'error', message: err.message });
+  }
+
+  console.error(err);
+
+  return res
+    .status(500)
+    .json({ status: 'error', message: 'Internal server error' });
+});
 
 app.listen(3333, () => {
   console.log('🐱‍🏍 Back-end started in 3333 port!');
