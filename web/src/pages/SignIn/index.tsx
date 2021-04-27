eb9e6191233d1d90a56c05cdb2d592c7ec9307bf
Breno Farias <brenofarias.sousa@gmail.com>
2021-04-27 10:00:37 -0300

    Estrutura de Toasts

--- web/src/pages/SignIn/index.tsx
+++ web/src/pages/SignIn/index.tsx
@@ -40,9 +40,10 @@ const SignIn: React.FC = () => {
 
         signIn({ email: data.email, password: data.password });
       } catch (err) {
-        const errors = getValidationErrors(err);
-
-        formRef.current?.setErrors(errors);
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+        }
       }
     },
     [signIn],
